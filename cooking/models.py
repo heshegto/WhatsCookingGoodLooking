f61917b81092025187c3328a6fef1cb796e3642d
heshegto <heshkin@gmail.com>
2023-09-19 01:21:03 +0300

    New model, and migrations

--- cooking/models.py
+++ cooking/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 
-class RecieptStep(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.TextField(null=True, blank=True)
+class ReceiptStep(models.Model):
+    receipt_name = models.CharField(max_length=128)
+    step_number = models.PositiveSmallIntegerField(default=1)
     required_elements = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to='product_images')
-    category = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='product_images', null=True, blank=True)
+    next_step = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    time_to_cook = models.TimeField()
+    is_last = models.BooleanField(default=True)
 
     def __str__(self):
-        return f'Продукт: {self.name} | Категория: {self.category.name} | id: ({self.pk})'
+        return f'Рецепт: {self.receipt_name} | Шаг приготовления: {self.step_number} | id: ({self.pk})'
