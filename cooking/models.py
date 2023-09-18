fc3709b67b62192385689cd7c33d1e9ad034c9af
heshegto <heshkin@gmail.com>
2023-09-18 19:04:17 +0300

    added new model. Added static and media dir to settings.py

--- cooking/models.py
+++ cooking/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class RecieptStep(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+    required_elements = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='product_images')
+    category = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name} | id: ({self.pk})'
